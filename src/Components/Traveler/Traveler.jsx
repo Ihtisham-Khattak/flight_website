64160fedde75437c3c3082d9c82f6d02ed0b49b4
Ihtisham Khattak <ikpashteen1@gmail.com>
2023-02-28 23:05:18 +0500

    last 3 modules remaining

--- src/Components/Traveler/Traveler.jsx
+++ src/Components/Traveler/Traveler.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Traveler = () => {
   return (
-    <div>Traveler</div>
-  )
-}
+    <div className="travelers container section">
+      <div className="section__container">
+        <h2>Top Travels of the Month</h2>
 
-export default Traveler
+        <div className="travelers__container grid">
+          <div className="single__travelers">
+            ,<img className="destination__image" src="" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Traveler;
